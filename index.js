647ca53935602926fd3a37a20d19b09e27f1100b
Code-Warlock <dessymichaeltgp123@gmail.com>
2021-07-28 10:45:37 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// alert("Welcome to JS");
+// confirm("Do you want to leave this page?");
+// prompt("Enter thy name ");
+// console.log("Hello Demilade and Favour");
+
+// let name = prompt("Enter your name");
+// const PASSWORD = prompt("Password please");
+// alert(name);
+// alert(password);
+let name = "Desmond";
+let age = 25;
+let tall = true;
